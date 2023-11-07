1bad3b77fc73c8291c97f5fe7c86cffd29722033
xavier <2139676@cstj.qc.ca>
2023-11-07 12:21:32 -0500

    infos de clarifications pour le profil

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,10 @@
 const Profile = () => {
+
+    // on va avoir besoin de récupérer les infos de l'utilisateur connecté
+    // aussi avoir les infos de lecture de l'utilisateur
+    // stocker les infos dans firebase
+    // favoris dans la base de données
+
     return (
         <div>
             <h1>Profile</h1>
