1ad2c12d326d24db0226da6726958d9db05b2af9
Tom Grimard <1578955@cstj.qc.ca>
2023-11-13 14:57:37 -0500

    creation accueil

---
+++ src/components/AffichageElements.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+const AffichageElements = ({categorie}) => {
+    const [categorie, setCategorie] = useState({});
+    return (
+        useEffect(() => {
+            const fetchByCategorie = async (categorie) => {
+                const resp = await fetch
+            }
+        })
+    );
+}
+export default AffichageElements;
