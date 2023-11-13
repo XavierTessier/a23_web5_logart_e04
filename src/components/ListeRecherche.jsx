d9d81ecb9d0d9d76ae7759c9f6d0b23c552a3cff
xavier <2139676@cstj.qc.ca>
2023-11-13 14:18:05 -0500

    écriture du user dans la base de donnée

--- src/components/ListeRecherche.jsx
+++ src/components/ListeRecherche.jsx
@@ -1,14 +1,17 @@
+import { useAuth } from "../context/authContext";
+
 const ListeRecherche = ({liste}) => {
+    const { addMusicToUser, user } = useAuth();
     return (
         <div className='results'>
                 {liste.data && liste.data.map((result) => (
                     <div className='card' key={result.id}>
                         <p className='title'>{result.title}</p>
                         <p className='artist'>{result.artist.name}</p>
-                        {console.log(result.artist)}
+                        {console.log(result)}
                         <p className='album'>{result.album.title}</p>
                         <img className='albumCover' src={result.album.cover_medium} alt=""/>
-                        <button>Like</button>
+                        <button onClick={addMusicToUser(user.uid, result.title, result.link)}>Like</button>
                     </div>
                 ))}
             </div>
--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState, useEffect } from 'react';
-import { auth } from '../config/firebase';
+import { auth, db } from '../config/firebase';
 import { signInWithPopup, signOut, onAuthStateChanged } from "firebase/auth";
+import { addDoc, collection, query, where } from 'firebase/firestore';
 
 
 
@@ -49,8 +50,50 @@ const AuthProvider = ({children}) => {
         } 
         
     }
+
+    const addDocHandler = async (uid, displayName) => {
+        try {
+            const docRef = await addDoc(collection(db, "users"), {
+                uid: uid,
+                displayName: displayName,
+                playlist: [],
+            });
+            console.log("Document written with ID: ", docRef.id);
+        } catch (e) {
+            console.error("Error adding document: ", e);
+        }
+    };
+
+    const addMusicToUser = async (uid, title, link) => {
+        const userQuery = query(collection(db, "users"), where("uid", "==", uid));
+        const userSnapshot = await getDocs(userQuery);
+    
+        if (userSnapshot.empty) {
+            console.error("Utilisateur non trouvé");
+            return;
+        }
+        const userDocRef = userSnapshot.docs[0].ref;
+        try {
+            const userData = (await getDoc(userDocRef)).data();
+    
+            // Vérifier si le champ "playlists" existe dans les données de l'utilisateur
+            const playlists = userData.playlists || [];
+    
+            // Ajouter la nouvelle playlist avec la musique
+            const newPlaylist = { timestamp: new Date().toISOString(), songs: [{ title, link }] };
+            playlists.push(newPlaylist);
+            // Mettre à jour le document utilisateur avec la nouvelle liste de playlists
+            await updateDoc(userDocRef, { playlists });
+    
+            console.log("Musique ajoutée à une nouvelle playlist avec succès");
+        } catch (error) {
+            console.error("Erreur lors de l'ajout de la musique à la nouvelle playlist:", error);
+        }
+    };
+
+
     return (
-        <Provider value = {{googleLogin, logout, user}}>
+        <Provider value = {{googleLogin, logout, user, addMusicToUser, addDocHandler}}>
             {children}         
         </Provider>
     );
