86768cb605bfddf1052d2fb5b70673168d328716
LAPTOP-Q088G62P\HP <1578955@cstj.qc.ca>
2023-11-13 23:52:16 -0500

    test fetch chart

--- src/components/AffichageElements.jsx
+++ src/components/AffichageElements.jsx
@@ -1,13 +1,35 @@
 import { useState } from "react";
+import { useEffect } from "react";
+import fetchJsonp from "fetch-jsonp";
 
-const AffichageElements = ({categorie}) => {
-    const [categorie, setCategorie] = useState({});
-    return (
-        useEffect(() => {
-            const fetchByCategorie = async (categorie) => {
-                const resp = await fetch
+const AffichageElements = ({categorieChercher}) => {
+    const [categorie, setCategorie] = useState([]);
+    useEffect(() => {
+        const fetchByCategorie = async () => {
+            try {
+                const resp = await fetchJsonp(`https://api.deezer.com/chart/${categorieChercher}s&output=jsonp`);
+                if(!resp) throw new Error('Network response was incorrect');
+                const data = await resp.json();
+                console.log(data)
+                setCategorie(data.data);
+            }catch (error){
+                console.error(`Error fetching ${categorieChercher}:`,error);
             }
-        })
+        };
+        fetchByCategorie();
+
+    },[]);
+    return (
+        <div>
+      <h1>Deezer Album Search</h1>
+      <ul>
+        {
+        
+        /* {categorie.map((element) => (
+          <li key={element.id}>untest</li>
+        ))} */}
+      </ul>
+    </div>
     );
 }
 export default AffichageElements;
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,6 +2,7 @@ import BarreRecherche from "./BarreRecherche";
 import Header from "./Header";
 import { Outlet } from "react-router";
 import { useAuth } from '../context/authContext';
+import AffichageElements from "./AffichageElements";
 
 const Layout = () => {
     const { user } = useAuth();
@@ -9,6 +10,7 @@ const Layout = () => {
         <div>
             <div>
                 <Header />
+                <AffichageElements categorieChercher="album"/>
                 {user ? (
                     <BarreRecherche />
                 ) : (
