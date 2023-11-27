e2cea3768d08136f308a9ab87746ae7716618f79
Chadrik Bertrand <2037079@cstj.qc.ca>
2023-11-27 10:28:26 -0500

    feature-css-navigation

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,29 +1,23 @@
 import BarreRecherche from "./BarreRecherche";
 import Header from "./Header";
 import { Outlet } from "react-router";
-import { useAuth } from '../context/authContext';
+import { useAuth } from "../context/authContext";
 import AffichageElements from "./AffichageElements";
 
 const Layout = () => {
-    const { user } = useAuth();
-    return (
-        <div>
-            <div>
-                <Header />
-                <Outlet />
-                {user ? (
-                    <BarreRecherche />
-                ) : (
-                    <></>
-                )}
-                <AffichageElements categorieChercher="chart" type="albums"/>
-                <AffichageElements categorieChercher="chart" type="artists"/>
-                <AffichageElements categorieChercher="chart" type="tracks"/>
-            </div>
-
-            
-        </div>
-    );
+  //   const { user } = useAuth();
+  return (
+    <div>
+      <div>
+        <Header />
+        <Outlet />
+        {/* {user ? <BarreRecherche /> : <></>} */}
+        <AffichageElements categorieChercher="chart" type="albums" />
+        <AffichageElements categorieChercher="chart" type="artists" />
+        <AffichageElements categorieChercher="chart" type="tracks" />
+      </div>
+    </div>
+  );
 };
 
 export default Layout;
