4f59c4ec64996e6764d6f90e71d542fad91b4ca8
LAPTOP-Q088G62P\HP <1578955@cstj.qc.ca>
2023-11-27 05:10:18 -0500

    EssaieAjoutIDTracksLayout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -16,8 +16,8 @@ const Layout = () => {
                 ) : (
                     <></>
                 )}
-                <AffichageElements categorieChercher="chart" type="albums"/>
-                <AffichageElements categorieChercher="chart" type="artists"/>
+                {/* <AffichageElements categorieChercher="chart" type="albums"/> */}
+                {/* <AffichageElements categorieChercher="chart" type="artists"/> */}
                 <AffichageElements categorieChercher="chart" type="tracks"/>
             </div>
 
