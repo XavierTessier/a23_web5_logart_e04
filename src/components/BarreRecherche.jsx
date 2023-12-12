e34ff307ea1674df0cdc68ac7e12c7b8a1854ea2
Chadrik Bertrand <2037079@cstj.qc.ca>
2023-12-11 22:13:47 -0500

    essais-toggle

--- src/components/BarreRecherche.jsx
+++ src/components/BarreRecherche.jsx
@@ -2,10 +2,9 @@ import fetchJsonp from "fetch-jsonp";
 import { useState, useEffect, useRef } from "react";
 import { useAuth } from "../context/authContext";
 import { Link } from "react-router-dom";
-
+import "../css/recherche.css";
 
 const BarreRecherche = () => {
-
   const [query, setQuery] = useState({
     value: "",
   });
@@ -56,16 +55,15 @@ const BarreRecherche = () => {
     console.log("type changed : " + type);
   }, [type]);
 
-  
   useEffect(() => {
     if (buttonClicked || type !== "") {
       const fetchResults = async () => {
         const resp = await fetchJsonp(
           `https://api.deezer.com/search/${type}?q=${query.value}&output=jsonp&limit=${limit}`
-          );
-          const data = await resp.json();
-          setResults(data);
-        };
+        );
+        const data = await resp.json();
+        setResults(data);
+      };
       fetchResults();
       setButtonClicked(false);
     }
@@ -74,7 +72,7 @@ const BarreRecherche = () => {
   useEffect(() => {
     const options = {
       root: null,
-      rootMargin: '0px',
+      rootMargin: "0px",
       threshold: 0.5,
     };
 
@@ -86,9 +84,12 @@ const BarreRecherche = () => {
     }, options);
 
     if (observer.current && results.data && results.data.length > 0) {
-      observer.current.observe(document.getElementById(`item-${results.data[results.data.length - 1].id}`));
+      observer.current.observe(
+        document.getElementById(
+          `item-${results.data[results.data.length - 1].id}`
+        )
+      );
     }
-
 
     // Cleanup the observer on component unmount
     return () => {
@@ -100,86 +101,176 @@ const BarreRecherche = () => {
 
   const displayList = (type) => {
     switch (type) {
-        case "album":
-            return (
-                <div className='results'>
-                        {results.data && results.data.map((result) => (
-                            <div className='card' key={result.id} id={`item-${result.id}`}>
-                                <p className='title'>{result.title}</p>
-                                <p className='artist'>{result.artist && result.artist.name}</p>
-                                <img className='albumCover' src={result.cover_medium} alt="" />
-                            </div>
-                        ))}
+      case "album":
+        return (
+          <div className="results">
+            {results.data &&
+              results.data.map((result) => (
+                <div className="card" key={result.id} id={`item-${result.id}`}>
+                  <p className="title">{result.title}</p>
+                  <p className="artist">
+                    {result.artist && result.artist.name}
+                  </p>
+                  <img
+                    className="albumCover"
+                    src={result.cover_medium}
+                    alt=""
+                  />
                 </div>
-            );
-        case "artist":
-            return (
-                <div className='results'>
-                    {results.data && results.data.map((result) => (
-                        <div className='card' key={result.id} id={`item-${result.id}`}>
-                            <p className='artist'>{result.name}</p>
-                            <img className='picture' src={result.picture_medium} alt="" />
-                        </div>
-                    ))}
+              ))}
+          </div>
+        );
+      case "artist":
+        return (
+          <div className="results">
+            {results.data &&
+              results.data.map((result) => (
+                <div className="card" key={result.id} id={`item-${result.id}`}>
+                  <p className="artist">{result.name}</p>
+                  <img className="picture" src={result.picture_medium} alt="" />
                 </div>
-            );
-        case "track":
-            return (
-                <div className='results'>
-                    {results.data && results.data.map((result) => (
-                        <div className='card' key={result.id} id={`item-${result.id}`}>
-                            <Link to={`reader/track/${result.id}`}>
-                                <p className='title'>{result.title}</p>
-                                <p className='artist'>{result.artist && result.artist.name}</p>
-                                <p className='album'>{result.album && result.album.title}</p>
-                                <img className='albumCover' src={result.album && result.album.cover_medium} alt="" />
-                            </Link>
-                            <button onClick={() => addMusicToUser(userData.playlist, { id: result.id, title: result.title, artist: result.artist.name, albumTitle: result.album.title, albumCover: result.album.cover_medium, duration: result.duration })}>Ajouter à la playlist</button>
-                            <button onClick={() => addToFav(userData.favorites, { id: result.id, title: result.title, artist: result.artist.name, albumTitle: result.album.title, albumCover: result.album.cover_medium, duration: result.duration })}>Fav</button>
-                            <button onClick={() => addToHistory({id: result.id, title: result.title, artist: result.artist.name, albumTitle: result.album.title, albumCover: result.album.cover_medium})}>historique</button>
-                        </div>
-                    ))}
+              ))}
+          </div>
+        );
+      case "track":
+        return (
+          <div className="results">
+            {results.data &&
+              results.data.map((result) => (
+                <div className="card" key={result.id} id={`item-${result.id}`}>
+                  <Link to={`reader/track/${result.id}`}>
+                    <p className="title">{result.title}</p>
+                    <p className="artist">
+                      {result.artist && result.artist.name}
+                    </p>
+                    <p className="album">
+                      {result.album && result.album.title}
+                    </p>
+                    <img
+                      className="albumCover"
+                      src={result.album && result.album.cover_medium}
+                      alt=""
+                    />
+                  </Link>
+                  <button
+                    onClick={() =>
+                      addMusicToUser(userData.playlist, {
+                        id: result.id,
+                        title: result.title,
+                        artist: result.artist.name,
+                        albumTitle: result.album.title,
+                        albumCover: result.album.cover_medium,
+                        duration: result.duration,
+                      })
+                    }
+                  >
+                    Ajouter à la playlist
+                  </button>
+                  <button
+                    onClick={() =>
+                      addToFav(userData.favorites, {
+                        id: result.id,
+                        title: result.title,
+                        artist: result.artist.name,
+                        albumTitle: result.album.title,
+                        albumCover: result.album.cover_medium,
+                        duration: result.duration,
+                      })
+                    }
+                  >
+                    Fav
+                  </button>
+                  <button
+                    onClick={() =>
+                      addToHistory({
+                        id: result.id,
+                        title: result.title,
+                        artist: result.artist.name,
+                        albumTitle: result.album.title,
+                        albumCover: result.album.cover_medium,
+                      })
+                    }
+                  >
+                    historique
+                  </button>
                 </div>
-            );
-        default:
-            return (
-                <div className='results'>
-                    {liste.data && liste.data.map((result) => (
-                        <div className='card' key={result.id} id={`item-${result.id}`}>
-                            <p className='title'>{result.title}</p>
-                            <p className='artist'>{result.artist && result.artist.name}</p>
-                            <p className='album'>{result.album && result.album.title}</p>
-                            <img className='albumCover' src={result.album && result.album.cover_medium} alt="" />
-                            <button onClick={() => addMusicToUser(userData.playlist, { id: result.id, title: result.title, artist: result.artist.name, albumTitle: result.album.title, albumCover: result.album.cover_medium, duration: result.duration })}>Ajouter à la playlist</button>
-                            <button onClick={() => addToFav(userData.favorites, { id: result.id, title: result.title, artist: result.artist.name, albumTitle: result.album.title, albumCover: result.album.cover_medium, duration: result.duration })}>Fav</button>
-                        </div>
-                    ))}
+              ))}
+          </div>
+        );
+      default:
+        return (
+          <div className="results">
+            {liste.data &&
+              liste.data.map((result) => (
+                <div className="card" key={result.id} id={`item-${result.id}`}>
+                  <p className="title">{result.title}</p>
+                  <p className="artist">
+                    {result.artist && result.artist.name}
+                  </p>
+                  <p className="album">{result.album && result.album.title}</p>
+                  <img
+                    className="albumCover"
+                    src={result.album && result.album.cover_medium}
+                    alt=""
+                  />
+                  <button
+                    onClick={() =>
+                      addMusicToUser(userData.playlist, {
+                        id: result.id,
+                        title: result.title,
+                        artist: result.artist.name,
+                        albumTitle: result.album.title,
+                        albumCover: result.album.cover_medium,
+                        duration: result.duration,
+                      })
+                    }
+                  >
+                    Ajouter à la playlist
+                  </button>
+                  <button
+                    onClick={() =>
+                      addToFav(userData.favorites, {
+                        id: result.id,
+                        title: result.title,
+                        artist: result.artist.name,
+                        albumTitle: result.album.title,
+                        albumCover: result.album.cover_medium,
+                        duration: result.duration,
+                      })
+                    }
+                  >
+                    Fav
+                  </button>
                 </div>
-            );
+              ))}
+          </div>
+        );
     }
-};
+  };
 
   return (
     <div>
       <form action="">
         <input
           type="text"
-          placeholder="Query for Api"
+          placeholder="Rechercher une chanson"
           onChange={updateState}
           value={query.value}
         />
         <button onClick={handleSearch} disabled={!isValid()}>
-          Search
+          Recherche
         </button>
 
-        <input
-          type="radio"
-          id="album"
-          name="type"
-          value="album"
-          onClick={handleType}
-        />
-        <label htmlFor="album">Album</label>
+        <label htmlFor="album">
+          <input
+            type="radio"
+            id="album"
+            name="type"
+            value="album"
+            onClick={handleType}
+          />
+          Album
+        </label>
 
         <input
           type="radio"
