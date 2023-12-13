fa66317ed6e5cefac6483e618cc41c5a41db526a
LAPTOP-Q088G62P\HP <1578955@cstj.qc.ca>
2023-12-13 18:39:45 -0500

    FixAffichageElementDone

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -16,9 +16,10 @@ const Layout = () => {
                 ) : (
                     <></>
                 )}
-                {/* <AffichageElements categorieChercher="chart" type="albums"/> */}
-        {/* <AffichageElements categorieChercher="chart" type="artists" />
-        <AffichageElements categorieChercher="chart" type="tracks" /> */}
+                */}
+        <AffichageElements categorieChercher="chart" type="albums"/>
+        <AffichageElements categorieChercher="chart" type="artists"/>
+        <AffichageElements categorieChercher="chart" type="tracks"/>
       </div>
     </div>
   );
